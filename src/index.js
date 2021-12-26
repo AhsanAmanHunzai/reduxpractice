f047efc69046a43ee3620a07ab82f3362aa14d14
AhsanAmanHunzai <ahsanaman76@gmail.com>
2021-12-27 02:24:42 +0500

    functions the first class citizens

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log("Hello World!");
+//  Senario #1
+function fn(){
+    return "Hello!";
+}
+
+let calling  = fn;
+calling(); // this will output  "Hello!"
+console.log(calling);  // this is calling the whole functions
+console.log(calling());  // this is calling the return functions
+
+//
+
+function fn1(){
+    return function(){
+        return "Hello";
+    }
+}
+let calling2;
+calling2 = fn1();
+
+console.log(calling2); // this should return the function
+console.log(calling2()); // this should return the string
+
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/index.js",   // This is the entry point for our code
   output: {
     filename: "app.js",
     path: path.resolve(__dirname, "dist")
   },
   devServer: {
-    contentBase: path.join(__dirname, "dist"),
+    contentBase: path.join(__dirname, "dist"),  // this is to tell the application to load from this folder "dist" at port 9000
     port: 9000
   },
   mode: "development"
