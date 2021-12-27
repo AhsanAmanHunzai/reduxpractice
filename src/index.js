80808e6b60fd71c9407155ed0e78429bb7d80074
AhsanAmanHunzai <ahsanaman76@gmail.com>
2021-12-27 16:35:32 +0500

    deep copy & shallow copy

--- src/index.js
+++
@@ -1,8 +0,0 @@
-// the old way of wraping stuff in <div> used to be like
-
-let input = "  Hello  "; // intentionally added spaces
-let output = `<div>${input.trim()}</div>`
-
-console.log(output)
-
-///
